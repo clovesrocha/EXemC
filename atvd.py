ee849fb4dbcbd81540dcb55cf95f5e8149261a6e
Micael Oliveira <166876575+micael022@users.noreply.github.com>
2024-04-24 00:53:45 -0300

    Create atvd.py

---
+++ atvd.py
@@ -0,0 +1,5 @@
+#algoritmo simples para calcular a média de dois números no terminal em py
+n1 = float(input('digite um número'))
+n2 = float(input('digite um número'))
+m = (n1 + n2) / 2
+print('a media entre {} e {} é {}'.format(n1, n2, m))
